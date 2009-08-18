6c06e1d9e9dfb8e59fecbc21796f9e06848e68d7
mr. blue <gautam.chekuri@azri.biz>
2009-08-18 21:00:38 +0530

    disable auth_token since uploads will happen via the applet

--- uploader/app/controllers/uploader_controller.rb
+++ uploader/app/controllers/uploader_controller.rb
@@ -1,8 +1,17 @@
 class UploaderController < ApplicationController
+  self.allow_forgery_protection = false
+  
   def index
   end
 
   def upload
-    logger.info( params.inspect )
+    logger.info("#################")
+    params[:payload].each {
+      |line|
+      logger.info( params[:payload].lineno.to_s + " : " + line)
+    }
+    logger.info("#################")
+    render :text => "OK"
   end
+  
 end
