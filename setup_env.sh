5b834c594c280ff018353ffccf43690d65d20c06
mr. blue <gautam.chekuri@azri.biz>
2009-08-29 17:20:44 +0530

    added a few comments about uage

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+### Usage: . ./setup_env.sh ############################################
+###### Be sure to source this script from the uploader dir #############
+### This is the dir where the Uploader.java and libs/ are located ######
+
 for jarfile in libs/*.jar; do
   export CLASSPATH=`pwd`/$jarfile:$CLASSPATH
 done
